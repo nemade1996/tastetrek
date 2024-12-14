fdecf1cce5c316c51d49c48fe8dcb7ee094733b0
Neha Nemade <81755618+nemade1996@users.noreply.github.com>
2024-12-14 14:09:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -120,6 +120,6 @@ const appRouter = createBrowserRouter([
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={appRouter} />);
+root.render(<RouterProvider router={appRouter} basename="/tastetrek" />);
 
 export default App;
